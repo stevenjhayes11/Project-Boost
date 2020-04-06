302dce68cfeb64f0e2031d9f200dee520db0acd0
stevenjhayes11 <41926543+stevenjhayes11@users.noreply.github.com>
2020-04-05 22:25:34 -0400

    Force and Rotaion
    Added vertical force and rotation

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        float rotation = 40 * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            rigidBody.AddRelativeForce(Vector3.up);
+            print("Space pressed");
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            print("Right pressed");
+            transform.Rotate(Vector3.back, rotation);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Left pressed");
+            transform.Rotate(Vector3.forward, rotation);
+        }
+    }
+}
